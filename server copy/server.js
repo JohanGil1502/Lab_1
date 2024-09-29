3c1557d3b4952ad9303867d96e7ab7dbf0c55a94
SebasGrosso <sebastian.gil01@uptc.edu.co>
2024-09-28 19:04:01 -0500

    Se agrega gestioń de logs

--- server copy/server.js
+++ server copy/server.js
@@ -3,17 +3,17 @@ const natural = require('natural');
 const cors = require('cors')
 const app = express();
 const port = 4002;
+const logs = [];
 
 app.use(cors());
 app.use(express.json());
 
 app.post('/countTokens', (req, res) => {
+    addLog("El usuario ha enviado: ", req.body.text);
     let words = req.body.text;
-    if (!words) {
-        return res.status(400).send('Error: Campo "text" no proporcionado.');
-    }
     const tokenizer = new natural.WordTokenizer();
     const tokens = tokenizer.tokenize(words);
+    addLog("Los tokens del texto son: ", tokens.length);
     res.send({ tokenCount: tokens.length });
 });
 
@@ -22,5 +22,18 @@ app.get('/isReady', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port}`);
+    addLog(`Servidor corriendo en el puerto ${port}`,"");
 });
+
+app.post('/sendLogs', (req, res) => {
+    addLog("Enviando array de logs","")
+    res.send(logs);
+});
+
+function addLog(message, content) {
+    const time = new Date();
+    const fecha = time.toLocaleDateString();
+    const hora = time.toLocaleTimeString();
+    console.log(fecha + " - "  + hora + " " + message + content);
+    logs.push(fecha + " - "  + hora + " " + message + content);
+}
--- server/server.js
+++ server/server.js
@@ -3,19 +3,17 @@ const natural = require('natural');
 const cors = require('cors')
 const app = express();
 const port = 4001;
+const logs = [];
 
 app.use(cors());
 app.use(express.json());
 
 app.post('/countTokens', (req, res) => {
-    console.log(req.body.text)
+    addLog("El usuario ha enviado: ", req.body.text);
     let words = req.body.text;
-    if (!words) {
-        return res.status(400).send('Error: Campo "text" no proporcionado.');
-    }
     const tokenizer = new natural.WordTokenizer();
     const tokens = tokenizer.tokenize(words);
-    console.log(tokens.length);
+    addLog("Los tokens del texto son: ", tokens.length);
     res.send({ tokenCount: tokens.length });
 });
 
@@ -23,6 +21,19 @@ app.get('/isReady', (req, res) => {
     res.send({ answer: 'yes' });
 });
 
-app.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port}`);
+app.post('/sendLogs', (req, res) => {
+    addLog("Enviando array de logs","")
+    res.send(logs);
 });
+
+app.listen(port, () => {
+    addLog(`Servidor corriendo en el puerto ${port}`,"");
+});
+
+function addLog(message, content) {
+    const time = new Date();
+    const fecha = time.toLocaleDateString();
+    const hora = time.toLocaleTimeString();
+    console.log(fecha + " - "  + hora + " " + message + content);
+    logs.push(fecha + " - "  + hora + " " + message + content);
+}
