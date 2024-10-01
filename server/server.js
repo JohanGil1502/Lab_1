7be3a070a52e71d6f68cadc969ed56c4e1799b35
SebasGrosso <sebastian.gil01@uptc.edu.co>
2024-10-01 17:53:35 -0500

    Logs

--- server/server.js
+++ server/server.js
@@ -9,11 +9,11 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/countTokens', (req, res) => {
-    addLog("El usuario ha enviado: ", req.body.text);
+    addLog("El usuario ha enviado: ", req.body.text, "POST", req.body);
     let words = req.body.text;
     const tokenizer = new natural.WordTokenizer();
     const tokens = tokenizer.tokenize(words);
-    addLog("Los tokens del texto son: ", tokens.length);
+    addLog("Los tokens del texto son: ", tokens.length, "POST", req.body);
     res.send({ tokenCount: tokens.length });
 });
 
@@ -22,18 +22,18 @@ app.get('/isReady', (req, res) => {
 });
 
 app.get('/sendLogs', (req, res) => {
-    addLog("Enviando array de logs","")
+    addLog("Enviando array de logs","" ,"GET"  )
     res.send(logs);
 });
 
 app.listen(port, () => {
-    addLog(`Servidor corriendo en el puerto ${port}`,"");
+    addLog(`Servidor corriendo en el puerto ${port}`,"","LISTEN");
 });
 
-function addLog(message, content) {
+function addLog(message, content, method, payload) {
     const time = new Date();
     const fecha = time.toLocaleDateString();
     const hora = time.toLocaleTimeString();
-    console.log(fecha + " - "  + hora + " " + message + content);
-    logs.push(fecha + " - "  + hora + " " + message + content);
+    console.log(fecha + " - "  + hora + " " + "lo"+ message + content + method +"Payload: "+payload);
+    logs.push(fecha + " - "  + hora + " " + +message + content + method+"Payload: "+payload);
 }
